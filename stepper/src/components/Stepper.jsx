922fff1e475fda2e8d8038fcf5054009da67ed32
siam114 <smsiamislam598@gmail.com>
2025-08-06 20:24:12 +0600

    complete design current step

--- stepper/src/components/Stepper.jsx
+++ stepper/src/components/Stepper.jsx
@@ -1,23 +1,59 @@
-const CheckoutStepper = ({stepsConfig = []}) => {
+import { useState } from "react";
 
-    if (!stepsConfig.length) {
-        return <></>
-    }
+const CheckoutStepper = ({ stepsConfig = [] }) => {
+  const [currentStep, setCurrentStep] = useState(1);
+  const [isComplete, setIsComplete] = useState(false);
+
+  if (!stepsConfig.length) {
+    return <></>;
+  }
+
+  const handleNext = () => {
+    setCurrentStep((prevStep) => {
+      if (prevStep === stepsConfig.length) {
+        setIsComplete(true);
+        return prevStep;
+      } else {
+        return prevStep + 1;
+      }
+    });
+  };
+
+  const ActiveComponent = stepsConfig[currentStep - 1]?.Component;
 
   return (
-    <div className="stepper">
-      {
-        stepsConfig.map((step, index) => {
+    <>
+      <div className="stepper">
+        {stepsConfig.map((step, index) => {
           return (
-            <div key={step.name} className="step">
-                <div className="step-number">{index + 1}</div>
-                <div className="step-name">{step.name}</div>
+            <div
+              key={step.name}
+              className={`step ${currentStep > index + 1 ? "complete" : ""} ${
+                currentStep === index + 1 ? "active" : ""
+              }`}
+            >
+              <div className="step-number">
+                {currentStep > index + 1 || isComplete ? (
+                  <span>&#10003;</span>
+                ) : (
+                  index + 1
+                )}
+              </div>
+              <div className="step-name">{step.name}</div>
             </div>
           );
-        })
-      }
-    </div>
-  )
-}
+        })}
+      </div>
 
-export default CheckoutStepper
+      <ActiveComponent />
+
+      {!isComplete && (
+        <button className="btn" onClick={handleNext}>
+          {currentStep === stepsConfig.length ? "Finish" : "Next"}
+        </button>
+      )}
+    </>
+  );
+};
+
+export default CheckoutStepper;
