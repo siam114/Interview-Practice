79a29d082cba98d26b9df887028e433fc51b90ec
siam114 <smsiamislam598@gmail.com>
2025-08-06 19:33:24 +0600

    added external css

--- stepper/src/components/Stepper.jsx
+++ stepper/src/components/Stepper.jsx
@@ -1,7 +1,21 @@
-const CheckoutStepper = () => {
+const CheckoutStepper = ({stepsConfig = []}) => {
+
+    if (!stepsConfig.length) {
+        return <></>
+    }
+
   return (
-    <div>
-      
+    <div className="stepper">
+      {
+        stepsConfig.map((step, index) => {
+          return (
+            <div key={step.name} className="step">
+                <div className="step-number">{index + 1}</div>
+                <div className="step-name">{step.name}</div>
+            </div>
+          );
+        })
+      }
     </div>
   )
 }
